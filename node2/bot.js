75734ecbe6bc7db0e0480b3e6ea6281b3ca69c55
Blonded <Lena@Lenas-MacBook-Pro.local>
2018-09-20 16:21:35 -0700

    added access token

--- node2/bot.js
+++ node2/bot.js
@@ -2,3 +2,22 @@
 
 // this npm package is required in order for the app to function
 var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+
+var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
+  });
+
+
+  client.get(path, params, callback);
+  client.post(path, params, callback);
+  client.stream(path, params, callback);
